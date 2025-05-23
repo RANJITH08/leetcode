8eb20d5992e5042707b0d7cdfce10949a9608276
RANJITH08 <ranjithstudent008@gmail.com>
2025-05-23 13:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (5.09%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -8,14 +8,14 @@ public:
 
       
         while (n) {
-            digits.push_back(n % 10);
+            digits.push_back(n % 10); // removing last val and pusing into the vector digits
             n /= 10;
         }
 
        
-        sort(digits.rbegin(), digits.rend());
+        sort(digits.rbegin(), digits.rend()); // descending ordeeerr
 
         
-        return digits[0] * digits[1];
+        return digits[0] * digits[1]; // product of 1st two digts 
     }
 };
