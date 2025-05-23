92b742fe628f198381ec38843bb7f2f597017118
RANJITH08 <ranjithstudent008@gmail.com>
2025-05-23 13:05:07 +0530

    Time: 1 ms (26.71%) | Memory: 10.7 MB (5.09%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,13 +1,21 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
     int maxProduct(int n) {
-        priority_queue<int>pq;
-        while(n){
-            pq.push(n%10);
-            n/=10;
+        vector<int> digits;
+
+      
+        while (n) {
+            digits.push_back(n % 10);
+            n /= 10;
         }
-        int temp = pq.top();
-        pq.pop();
-        return temp*pq.top();
+
+       
+        sort(digits.rbegin(), digits.rend());
+
+        
+        return digits[0] * digits[1];
     }
 };
